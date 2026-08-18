33e55f4738a16158a64fb0549445335e3a2424b3
爱吃罗氏虾 <1401647713@qq.com>
2026-08-18 18:11:12 +0800

    已完成死信方面的初始化配置；

--- src/main/java/com/example/config/RabbitTopologyConfig.java
+++ src/main/java/com/example/config/RabbitTopologyConfig.java
@@ -1,6 +1,6 @@
 package com.example.config;
 
-下 import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -28,14 +28,24 @@ public class RabbitTopologyConfig {
      */
     public static final String DEMO_ROUTING_KEY = "demo.hello";
 
+    /*
+     * ==============================
+     * 死信 RabbitMQ 配置
+     * ==============================
+     */
+    // 死信交换机
+    public static final String DEAD_EXCHANGE = "demo.dlx.exchange";
+    // 死信队列
+    public static final String DEAD_QUEUE = "demo.dlq.queue";
+    // 死信Routing key
+    public static final String DEAD_ROUTING_KEY = "demo.dead";
+
     /**
-     * 声明 DirectExchange。
-     * durable(true)：
-     * RabbitMQ 重启后，Exchange 仍然存在。
+     * 创建正常 Exchange
      */
     @Bean
     public DirectExchange demoExchange() {
-        return ExchangeBuilder.directExchange(DEMO_EXCHANGE).durable(true).build();
+        return new DirectExchange(DEMO_EXCHANGE, true, false);
     }
 
     /**
@@ -46,10 +56,10 @@ public class RabbitTopologyConfig {
      * Queue 持久化不等于消息一定持久化。
      * 后续还要给消息设置 persistent。
      */
-    @Bean
+    /*@Bean
     public Queue demoQueue() {
         return QueueBuilder.durable(DEMO_QUEUE).build();
-    }
+    }*/
 
     /**
      * 把 Queue 绑定到 Exchange。
@@ -57,40 +67,48 @@ public class RabbitTopologyConfig {
      */
     @Bean
     public Binding demoBinding(DirectExchange demoExchange, Queue demoQueue) {
-
         return BindingBuilder.bind(demoQueue).to(demoExchange).with(DEMO_ROUTING_KEY);
     }
 
+    /**
+     * ==============================
+     * 创建正常业务 Queue
+     * ==============================
+     * <p>
+     * 这里是死信最关键的地方。
+     */
+    @Bean
+    public Queue demoQueue() {
+        // 如果 DEMO_QUEUE 中的消息变成死信，就把消息发送给：demo.dlx.exchange 并使用 Routing Key：demo.dead；
+        return QueueBuilder.durable(DEMO_QUEUE)
+                .deadLetterExchange(DEAD_EXCHANGE)
+                .deadLetterRoutingKey(DEAD_ROUTING_KEY)
+                .build();
+    }
 
-    /*
-     * ==============================
-     * 死信 RabbitMQ 配置
-     * ==============================
-     */
     /**
-     * 死信交换机
+     * 创建死信 Exchange
+     * <p>
+     * 注意：DLX 本质就是普通 Exchange,我们这里继续使用 DirectExchange。
      */
-    public static final String DEAD_EXCHAGE = "demo.dlx.exchage";
-    /**
-     * 死信队列
-     */
-    public static final String DEAD_QUEUE = "demo.dlq.exchange";
-    /**
-     * 死信Rounting key
-     *
-     */
-    public static final String DEAD_ROUTING_KEY = "demo.dead";
-    /**
-     * ==============================
-     * 2. 创建死信 Exchange
-     * ==============================
-     *
-     * 注意：
-     *
-     * DLX 本质就是普通 Exchange。
-     *
-     * 我们这里继续使用 DirectExchange。
-     */
+    @Bean
+    public DirectExchange deadExchange() {
+        return new DirectExchange(DEAD_EXCHANGE, true, false);
+    }
 
+    /**
+     * 创建死信 Queue
+     */
+    @Bean
+    public Queue deadQueue() {
+        return QueueBuilder.durable(DEAD_QUEUE).build();
+    }
 
+    /**
+     * 创建死信 Binding
+     */
+    @Bean
+    public Binding deadBinding() {
+        return BindingBuilder.bind(deadQueue()).to(deadExchange()).with(DEAD_ROUTING_KEY);
+    }
 }
